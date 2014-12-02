6213a5ee0f20e96508d1749c28c6add26e8a429b
ido <idok@wix.com>
2014-12-02 14:30:06 +0200

    error handling + test

--- tasks/react-templates.js
+++ tasks/react-templates.js
@@ -16,15 +16,31 @@ module.exports = function register(grunt) {
 
 //            console.log(files);
 
-            files.forEach(function convert(file) {
-                try {
-                    var outFile = file + '.js';
-                    reactTemplates.convertFile(file, outFile);
-                    grunt.log.writeln('converting file ' + file + ' => ' + outFile);
-                } catch (e) {
-                    grunt.log.writeln('Error converting: ' + file + ', ' + e);
-                }
-            });
+//            options._ = this.data.src; // set positional arguments
+            conf._ = files;
+            var ret = reactTemplates.executeOptions(conf);
+            return ret === 0;
+//        options.config = options.config ? path.resolve(options.config) : '';
+
+//        grunt.log.writeln('this.filesSrc: ' + this.filesSrc);
+//        grunt.log.writeln('options: ' + JSON.stringify(options, undefined, 2));
+
+//            var packages = require('./lib/cli');
+//            return packages.execute(options) === 0;
+
+//            files.forEach(function convert(file) {
+//                try {
+//                    var outFile = file + '.js';
+//                    var context = reactTemplates.context;
+//                    reactTemplates.convertFile(file, outFile, conf, context);
+//                    context.getMessages().forEach(function (m) {
+//                        grunt.log.writeln('converting file ' + file + ' => ' + outFile);
+//                    });
+//                    grunt.log.writeln('converting file ' + file + ' => ' + outFile);
+//                } catch (e) {
+//                    grunt.log.writeln('Error converting: ' + file + ', ' + e);
+//                }
+//            });
         } catch (e) {
             grunt.log.writeln('Error: ' + e);
         }
