2ab0fb0d33312cd6c61adb1065cd5cb5c37b2215
idok <idok@wix.com>
2018-09-02 13:44:25 +0300

    add lint

--- tasks/react-templates.js
+++ tasks/react-templates.js
@@ -1,18 +1,19 @@
-'use strict';
+'use strict'
 module.exports = function register(grunt) {
     grunt.registerMultiTask('reactTemplates', function () {
-      try {
-        var rtOptions = this.options();
-        var path = require('path');
-        rtOptions._ = this.filesSrc.map(function(file) { return path.resolve(file)});
-        var exitCode = require('react-templates').executeOptions(rtOptions);
-        grunt.log.ok('processed ' + this.filesSrc.length + ' file' + (this.filesSrc.length > 1 ? 's' : ''));
-        return exitCode === 0;
-      } catch (e) {
-        grunt.log.error('Error: ' + e + ' ' + e.stack());
-      }
-    });
-    grunt.registerTask('react-templates', function() {
-      grunt.task.run(['reactTemplates'].concat(this.args).join(':'));
-    });
-};
+        try {
+            const rtOptions = this.options()
+            const path = require('path')
+            rtOptions._ = this.filesSrc.map(file => path.resolve(file))
+            const exitCode = require('react-templates').executeOptions(rtOptions)
+            grunt.log.ok(`processed ${this.filesSrc.length} file${this.filesSrc.length > 1 ? 's' : ''}`)
+            return exitCode === 0
+        } catch (e) {
+            grunt.log.error(`Error: ${e} ${e.stack()}`)
+        }
+    })
+
+    grunt.registerTask('react-templates', function () {
+        grunt.task.run(['reactTemplates'].concat(this.args).join(':'))
+    })
+}
